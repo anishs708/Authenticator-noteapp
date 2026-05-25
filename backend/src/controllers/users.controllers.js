70a187552e959985fdaa69385a4ba6656f6d9e76
anishs708 <anishs708@gmail.com>
2026-05-25 17:55:03 -0500

    did some changes to controller with jsonwebtoken during singup and login

--- backend/src/controllers/users.controllers.js
+++ backend/src/controllers/users.controllers.js
@@ -1,13 +1,28 @@
-import user from "../models/users.model.js";
+import User from "../models/users.model.js";
 import jwt from "jsonwebtoken";
 
-const signUser = async (req,res){
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
+
+const signUser = async (req,res)=>{
     const {email, password, name, age} = req.body;
     try{
-        const user = await user.signIn(email, password, name, age);
-        const token = create
+        const user = await User.signUp(email, password, name, age);
+        const token = createToken(user._id);
         res.status(200).json({email,token});
     }
-    catch(err){
+    catch(error){
+        res.status(400).json({error:error.message});
+    }
+}
+const logUser = async (req,res)=>{
+    const{email,password} = req.body;
+    try{
+        const user = await User.logIn(email,password);
+        const token = createToken(user._id);
+        res.status(200).json({email,token});
+    }catch(error){
+        res.status(400).json({error:error.message})
     }
 }
